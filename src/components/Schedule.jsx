ad3178e0d6be2865e3af41d975f570d860ea4e8a
Rei Sikk <reix0010@stud.kea.dk>
2022-12-06 20:45:49 +0100

    general layouting - grid and flex

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Schedule() {
   return (
-    <section>
+    <section className="schedule-comp">
       {" "}
       <h2>bands playing here</h2>
       <button>Click here to see</button>
