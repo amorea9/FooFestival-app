a0ce90f54db6577b12a79dc44c9c38517cda75d6
Rei Sikk <reix0010@stud.kea.dk>
2022-12-07 15:51:56 +0100

    bottom nav styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import BandPage from "./components/Pages/BandPage";
 import FavouritesPage from "./components/Pages/FavouritesPage";
 import LandingPage from "./components/Pages/LandingPage";
 import ProgramPage from "./components/Pages/ProgramPage";
+import BottomNav from "./components/Reusable components/BottomNav";
 import "./index.scss";
 
 function App() {
@@ -14,27 +15,11 @@ function App() {
           <li>en/dk</li>
         </ul>
       </nav>
-      <nav className="bottom-menu">
-        <ul>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Favourites</a>
-          </li>
-          <li>
-            <a href="">Schedule</a>
-          </li>
-          <li>
-            <a href="">Live now</a>
-          </li>
-        </ul>
-      </nav>
-
       <LandingPage />
       <ProgramPage />
       <FavouritesPage />
       <BandPage />
+      <BottomNav />
     </div>
   );
 }
