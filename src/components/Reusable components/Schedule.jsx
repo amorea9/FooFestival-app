475341e8a77da78b6c15249412c247c2b7515cb6
Alessia <98785381+amorea9@users.noreply.github.com>
2022-12-08 15:09:26 +0100

    replaced sections with articles for smaller comp

--- src/components/Reusable components/Schedule.jsx
+++ src/components/Reusable components/Schedule.jsx
@@ -1,10 +1,10 @@
 function Schedule() {
   return (
-    <section className="schedule-comp">
+    <article className="schedule-comp">
       {" "}
       <h2>bands playing here</h2>
       <button>Click here to see</button>
-    </section>
+    </article>
   );
 }
 
--- src/components/Reusable components/TodaysHeadliners.jsx
+++ src/components/Reusable components/TodaysHeadliners.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function TodaysHeadliners() {
   return (
-    <section className="todays-headliners-comp">
+    <article className="todays-headliners-comp">
       <h2>Today's Headliners</h2>
       <p>text here</p>
       <button>Click here to see</button>
-    </section>
+    </article>
   );
 }
 
