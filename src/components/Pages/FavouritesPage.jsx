02360424625ff2da5fab4f33f5ade9afa5d49199
Alessia <98785381+amorea9@users.noreply.github.com>
2022-12-08 14:43:35 +0100

    replaced some grids with flexes

--- src/components/Pages/FavouritesPage.jsx
+++ src/components/Pages/FavouritesPage.jsx
@@ -4,12 +4,12 @@ import BandsList from "../Reusable components/BandsList";
 function Favourites() {
   //remember to add "if no favourite" option to display a message
   return (
-    <div className="bands-list-wrapper">
+    <section className="bands-list-wrapper">
       <h1>Your favourites</h1>
       <p>search icon here</p>
       <BandsList />
       <BandsList />
-    </div>
+    </section>
   );
 }
 
