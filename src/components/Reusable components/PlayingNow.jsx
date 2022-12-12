35a9c8b0d1b1a60281bc623681da00ee573e2201
Alessia <98785381+amorea9@users.noreply.github.com>
2022-12-12 11:46:08 +0100

    added playing now message if no acts

--- src/components/Reusable components/PlayingNow.jsx
+++ src/components/Reusable components/PlayingNow.jsx
@@ -1,27 +1,43 @@
 import React from "react";
 
 function PlayingNow(props) {
-  return (
-    <article id="playingNow" className="playing-now-comp">
-      <h2>
-        Playing now <span> </span>
-      </h2>
-      <div className="playing-now-content">
-        <article className="playing-now-band">
-          <h3>{props.liveNow.jotunheimLiveState}</h3>
-          <p>Jotunheim stage</p>
-        </article>
-        <article className="playing-now-band">
-          <h4>{props.liveNow.midgardLiveState}</h4>
-          <p>Midgard stage</p>
-        </article>
-        <article className="playing-now-band">
-          <p>{props.liveNow.vanaheimLiveState}</p>
-          <p>Vanaheim stage</p>
-        </article>
-      </div>
-    </article>
-  );
+  if (props.liveNow.liveShowsAtm === false) {
+    return (
+      <article id="playingNow" className="playing-now-comp">
+        <h2>
+          Playing now <span> </span>
+        </h2>
+        <div className="playing-now-none">
+          <article>
+            <h3>Currently there is no live acts</h3>
+            <p>The next live acts will begin at 16:00 at all stages</p>
+          </article>
+        </div>
+      </article>
+    );
+  } else {
+    return (
+      <article id="playingNow" className="playing-now-comp">
+        <h2>
+          Playing now <span> </span>
+        </h2>
+        <div className="playing-now-content">
+          <article className="playing-now-band">
+            <h3>{props.liveNow.jotunheimLiveState}</h3>
+            <p>Jotunheim stage</p>
+          </article>
+          <article className="playing-now-band">
+            <h4>{props.liveNow.midgardLiveState}</h4>
+            <p>Midgard stage</p>
+          </article>
+          <article className="playing-now-band">
+            <p>{props.liveNow.vanaheimLiveState}</p>
+            <p>Vanaheim stage</p>
+          </article>
+        </div>
+      </article>
+    );
+  }
 }
 
 export default PlayingNow;
