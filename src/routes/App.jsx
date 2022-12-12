44c6b5311d0d85c2860ee33933eba63ad61cfa2b
Alessia <98785381+amorea9@users.noreply.github.com>
2022-12-12 11:34:47 +0100

    added pull request live now code

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LoginPage from "../components/Pages/LoginPage";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BottomNav from "../components/Reusable components/BottomNav";
 import "../sass/style.scss";
 import { Outlet } from "react-router-dom";
@@ -8,9 +8,8 @@ import TopNav from "../components/Reusable components/TopNav";
 
 function App() {
   //state used to determine if the user is logged in or not. it's faked
-  const [login, setLogin] = useState(false);
 
-  //TODO: add styling to band page
+  const [login, setLogin] = useState(false);
 
   return (
     <div className="layout-wrapper">
