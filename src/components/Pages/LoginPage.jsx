27c827b6f67fc35073f98772a92c7d9343f7ef57
Rei Sikk <reix0010@stud.kea.dk>
2022-12-13 16:03:02 +0100

    validation added-no styling yet

--- src/components/Pages/LoginPage.jsx
+++ src/components/Pages/LoginPage.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage(props) {
   const navigate = useNavigate();
+  //handle ticketnr input
+  const ticketNr = document.querySelector("input[id=ticketNumber]");
+  const logInBtn = document.querySelector("input[id=log-in-button]");
+  console.log(logInBtn);
+  const [ticketInput, setTicketInput] = useState("");
   function submit(e) {
-    e.preventDefault();
+    if (ticketNr.value.length > 8) {
+      e.preventDefault();
+      props.setLogin(true);
+      navigate("/landing-page#top");
+    }
   }
+  const handleTicketNr = (event) => {
+    console.log(event.target.value);
+    const result = event.target.value.toUpperCase();
+    setTicketInput(result);
+    if (ticketNr.value.length > 8) {
+      logInBtn.focus();
+    }
+  };
+
   return (
     <section className="log-in-page">
       <h1>Welcome to FooFest 2023</h1>
@@ -13,20 +32,40 @@ function LoginPage(props) {
           <h3>Log in</h3>
           <div className="email-input">
             <label htmlFor="email"> Email </label>
-            <input id="email" type="email" placeholder="JohnDoe@gmail.com" />
+            <input
+              required
+              aria-required="true"
+              autoFocus
+              id="email"
+              type="email"
+              name="email"
+              placeholder="JohnDoe@gmail.com"
+            />
           </div>
           <div className="ticket-number-input">
             <label htmlFor="ticketNumber">Ticket number </label>
-            <input id="ticketNumber" type="text" placeholder="AID74RBSM8Y" />
+            <input
+              required
+              aria-required="true"
+              id="ticketNumber"
+              type="text"
+              name="ticketNumber"
+              onChange={handleTicketNr}
+              minLength={9}
+              maxLength={9}
+              value={ticketInput}
+              placeholder="AID74RBSM"
+            />
           </div>
 
           <input
             className="log-in-button"
+            id="log-in-button"
+            name="log-in-button"
             type="submit"
             value="Log in &#10132;"
             onClick={() => {
-              props.setLogin(true);
-              navigate("/landing-page#top");
+              submit();
             }}
           />
         </form>
